33ae33dea006cd14fd405f0dc70078ca690cba8d
Maria Lirio Pardo <liriopf@hotmail.com>
2018-11-16 20:40:35 +0100

    Se añaden test

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,36 @@
 package tudelft.ghappy;
 
+import com.sun.xml.internal.ws.developer.UsesJAXBContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private GHappy happy;
+
+    @BeforeEach
+    public void createGHappy(){
+        happy = new GHappy();
+    }
+
+    @Test
+    public void testHappy(){
+        Assertions.assertTrue(happy.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void testUnHappy() {
+        Assertions.assertFalse(happy.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void testNotAllHappy() {
+        Assertions.assertFalse(happy.gHappy("xxggyygxx"));
+    }
+
+    @Test
+    public void testAllHappy() {
+        Assertions.assertTrue(happy.gHappy("gg"));
+    }
 }
