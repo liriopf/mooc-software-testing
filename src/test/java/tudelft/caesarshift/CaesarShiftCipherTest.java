14ed755a165b0be62787354e266e749ae2eb7653
Maria Lirio Pardo <liriopf@hotmail.com>
2018-11-16 21:01:17 +0100

    Se elimina linea que conquetaba string original

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,27 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
+
+    private CaesarShiftCipher encripter;
+
+    @BeforeEach
+    public void createCaesarShiftCipher(){
+        encripter = new CaesarShiftCipher();
+    }
+
+
+    @ParameterizedTest(name = "message={0}, shift={1}, result={2}")
+    @CsvSource({ "abc,3,def", "xyz,3,abc", "yza,3,bcd" })
+    public void encripterFirstLetters(String message, int shift, String expectedResult){
+        String result = encripter.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expectedResult,result);
+    }
 
 }
